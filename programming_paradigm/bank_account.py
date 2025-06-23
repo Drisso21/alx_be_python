efc320787ac5d495dc33abf5c24e63f371450fee
idriss solhi <93015866+Drisso21@users.noreply.github.com>
2025-06-23 11:59:55 +0200

    updatess

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,8 +4,11 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += float(amount)
-    def withdraw(self, amount):
-        if 0 < amount  < self.account_balance:
+    def withdraw(self, amount): 
+        if 0 < amount  <= self.account_balance:
             self.account_balance -= float(amount)
+            return True
+        else:
+            return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
