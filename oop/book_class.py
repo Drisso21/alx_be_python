24abb1e86683d1ea7857c492d15fbc0e58e3b4d0
idriss solhi <93015866+Drisso21@users.noreply.github.com>
2025-06-24 01:11:31 +0200

    new tg

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,6 +6,6 @@ class Book:
     def __del__(self):
         print(f'Deleting book {self.title}')
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}',{self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
